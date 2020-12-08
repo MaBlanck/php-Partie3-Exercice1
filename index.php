f28a197dc22868f4d140f8b6b73056f0584ce0e8
Max <maxblanck@outlook.fr>
2020-12-08 14:54:46 +0100

    php Partie 3  Exercice 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+$number = 0;
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 1 Partie 3</title>
+</head>
+<body>
+    <h1>Exercice 1 Partie 3</h1>
+
+    <p><?php 
+    while ($number <= 10)
+    {
+        echo $number;
+        $number++;
+    }
+    ?></p>
+</body>
+</html>
